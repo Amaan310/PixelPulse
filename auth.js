c059b1653103534cfc2ec1facb0e0171b692f597
Amaan Haque <amaanhaq77@gmail.com>
2025-10-27 11:14:27 +0530

    changed one line to auth.js

--- auth.js
+++ auth.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 // --- END: FIREBASE INITIALIZATION ---
